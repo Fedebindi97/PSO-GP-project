5ce81af814a4e157060b8f5a2bd20fc9eea3e7c7
fedebindi <82228286+Fedebindi97@users.noreply.github.com>
2024-12-17 09:08:03 +0100

    Setting up GP part

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ import copy
 from scipy.optimize import minimize
 np.random.seed(16021997)
 import yfinance as yf
-import copy
+import random
+import operator
+from deap import base, creator, tools, gp
 
 plt.rcParams['font.family'] = 'Monospace'
 plt.rcParams['axes.grid'] = True
@@ -56,3 +58,53 @@ companies_colors = {
     'TRN':'#285cbc', 
     'UCG':'#e41b23'
 }
+
+def fitness_function(weights,
+                     avg_returns,
+                     cov_matrix,
+                     risk_aversion):
+    weights = np.asarray(weights).reshape((-1, 1)) # to ensure matrix multiplications work
+    portfolio_return = np.dot(weights.T, avg_returns)
+    portfolio_variance = np.dot(weights.T, np.dot(cov_matrix, weights))
+    fitness = portfolio_return - (risk_aversion/2) * portfolio_variance
+    return -fitness.item() # minimization for scipy
+
+def markowitz_solution(num_stocks,
+                        avg_returns,
+                        cov_matrix,
+                        risk_aversion,
+                        short_selling=False):
+    
+    if not short_selling:
+        constraints = {'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}
+
+        # Bounds: each weight should be between 0 and 1 (no short-selling)
+        bounds = [(0, 1) for _ in range(num_stocks)]
+
+        # Initial guess (equal distribution)
+        initial_weights = np.ones(num_stocks) / num_stocks
+
+        # Optimization
+        result = minimize(fitness_function, 
+                        initial_weights, 
+                        method='SLSQP', 
+                        bounds=bounds, 
+                        constraints=constraints,
+                        args=(avg_returns, cov_matrix, risk_aversion),
+                        options={'maxiter':100000000,'ftol': 1e-8})
+
+        # Optimal weights and fitness value
+        optimal_weights_markowitz = result.x
+        optimal_fitness_markowitz = -result.fun  # Flip the sign back
+
+    else:
+        ones_vector = np.ones((num_stocks,1))
+        inv_cov_matrix = np.linalg.inv(cov_matrix)
+        R = inv_cov_matrix - np.dot(inv_cov_matrix,np.dot(ones_vector,np.dot(ones_vector.T,inv_cov_matrix))) / np.dot(ones_vector.T,np.dot(inv_cov_matrix,ones_vector))
+        optimal_weights_markowitz = np.array(np.dot(inv_cov_matrix,ones_vector) / np.dot(ones_vector.T,np.dot(inv_cov_matrix,ones_vector)) + np.dot(R,avg_returns)/risk_aversion)
+        optimal_fitness_markowitz = fitness_function(optimal_weights_markowitz,
+                                                     avg_returns,
+                                                    cov_matrix,
+                                                    risk_aversion)*-1
+
+    return optimal_weights_markowitz, optimal_fitness_markowitz
